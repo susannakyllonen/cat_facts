fc02dd1e1824e02901eaa2ea7d81ed6a11962785
Susanna K <susanna@twigthecode.com>
2020-07-20 11:10:57 +0300

    App fetches data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, Fragment } from "react";
+import Spinner from "react-bootstrap/Spinner";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+const App = () => {
+  const [data, setData] = useState([]);
+  const [loadingData, setLoadingData] = useState(false);
+
+  useEffect(() => {
+    fetchdata();
+  }, []);
+
+  const fetchdata = async () => {
+    setLoadingData(true);
+    const response = await fetch(
+      "https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=5"
+    );
+    const data = await response.json();
+    console.log(data);
+    setData(data);
+    setLoadingData(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Container className="container">
+        <Row>
+          <Col>
+            {loadingData && <Spinner animation="border" variant="primary" />}
+            <h1 style={{ color: "green" }} className="headline">
+              Cat Facts
+            </h1>
+
+            {data.map(facts => {
+              return (
+                <Card className="infotext">
+                  <Card.Body key={facts.id}>{facts.text}</Card.Body>
+                </Card>
+              );
+            })}
+            {!loadingData && (
+              <Button
+                className="buttonMargin"
+                variant="primary"
+                size="lg"
+                onClick={() => fetchdata()}
+              >
+                More Cat Facts!
+              </Button>
+            )}
+          </Col>
+        </Row>
+      </Container>
+    </Fragment>
   );
-}
+};
 
 export default App;
